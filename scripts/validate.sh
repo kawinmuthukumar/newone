d916bdeeec47ff93de05d24eff4dd184f82bdfe3
kawinmuthukumar <151517013+kawinmuthukumar@users.noreply.github.com>
2025-08-29 14:24:27 +0530

    Update validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-sleep 5
-if docker ps | grep -q my-nginx; then
-  echo "Nginx container is running ✅"
+set -e
+
+echo "Validating service..."
+
+sleep 5  # wait for nginx to start
+
+STATUS_CODE=$(curl -o /dev/null -s -w "%{http_code}" http://localhost)
+
+if [ "$STATUS_CODE" -eq 200 ]; then
+  echo "Validation passed! App is running."
   exit 0
 else
-  echo "Nginx container failed ❌"
+  echo "Validation failed! HTTP status: $STATUS_CODE"
   exit 1
 fi
